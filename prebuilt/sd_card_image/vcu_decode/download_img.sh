7ff40de8896a5dc75eb7a58b246e204f352e99c7
alex0620ee05 <75009861+alex0620ee05@users.noreply.github.com>
2020-12-07 16:50:34 +0800

    Add files via upload

---
+++ prebuilt/sd_card_image/vcu_decode/download_img.sh
@@ -0,0 +1,1 @@
+wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pITisw0bbiOIiply6RtszrXkeElyjIXf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1pITisw0bbiOIiply6RtszrXkeElyjIXf" -O sd_card.img && rm -rf cookies.txt
